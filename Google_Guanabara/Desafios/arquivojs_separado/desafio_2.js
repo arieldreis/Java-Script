ee372dae6fc0ea10c3a48e61f6b755b51da85905
Ariel Marinho <arieldreis@gmail.com>
2024-11-07 19:05:08 -0300

    Remoção
    Removi esses arquivos dessas pastas, e reloquei para outra pasta.

--- Google_Guanabara/Desafios/arquivojs_separado/desafio_2.js
+++
@@ -1,6 +0,0 @@
-function amostra(){
-    let nome = window.prompt('Qual é o seu nome?')
-    let idade = window.prompt(`Olá ${nome}! Quantos anos você tem? `)
-    window.alert(`Acabei de conhecer ${nome}, que tem ${idade} anos de idade!`)
-}
-    
--- Google_Guanabara/Desafios/arquivojs_separado/desafio_3.js
+++
@@ -1,6 +0,0 @@
-function sucessor(){
-    let num = parseInt(window.prompt('Digite um número inteiro qualquer: '));
-    let antecessor = (num -1)
-    let sucesso = (num + 1)
-    let resolucao = window.alert(`Antes de ${num} temos o número ${antecessor}\nDepois de ${num}, temos o número ${sucesso}`)
-}

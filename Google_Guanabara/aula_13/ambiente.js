6ae99e0a221583f1f3b5473d3c01b2284baa18ae
Ariel Marinho <arieldreis@gmail.com>
2024-11-07 18:45:05 -0300

    Java Script

---
+++ Google_Guanabara/aula_13/ambiente.js
@@ -0,0 +1,13 @@
+let c = 1;
+while(c <= 5){
+    console.log('Macaco')
+    c++
+}
+
+/*
+console.log('TUDO BEM!');
+console.log('TUDO BEM!');
+console.log('TUDO BEM!');
+console.log('TUDO BEM!');
+console.log('TUDO BEM!');
+*/
---
+++ Google_Guanabara/aula_14/ambiente.js
@@ -0,0 +1,12 @@
+// c = 1
+// while(c <= 5){
+//     console.log(`${c}`)
+//     c+=1
+// }
+
+//----------------------------------
+console.log('Vai Começar!')
+for(let c = 1; c <= 10;c+=1){
+    console.log(c)
+}
+console.log('FIM')

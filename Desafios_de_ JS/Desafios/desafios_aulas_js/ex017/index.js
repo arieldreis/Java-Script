2321019044df3ae9c768d7093457fb0ebae56348
Ariel Marinho <arieldreis@gmail.com>
2024-11-09 23:22:27 -0300

    Tabuada

--- Desafios_de_ JS/Desafios/desafios_aulas_js/ex017/index.js
+++ Desafios_de_ JS/Desafios/desafios_aulas_js/ex017/index.js
@@ -1,14 +1,22 @@
-function tabuada(evente){
-    evente.preventeDefault();
-    let tabuada = parseInt(document.getElementById('numero_mensagem').value);
+function tabuada(event){
+    event.preventDefault();
+    let tabuada = document.getElementById('numero_mensagem');
     let resolucao = document.getElementById('resolucao');
-    let contador = 1
-    let resultado = ""
-    do{
-        mult = contador * tabuada
-        resultado+= `${tabuada} x ${contador} = ${mult}<br>`
-        contador+=1
-    }while(contador <= 10)
-    resolucao.style.fontFamily = "Arial";
-    resolucao.innerHTML = resultado;
- }
+    let select = document.getElementById('select');
+    if(tabuada.value.length == 0){
+        window.alert('Campo vazio, digite um número para o seu funcionamento.');
+    }else if(isNaN(tabuada.value)){
+        window.alert('O número digitado não é numérico!');
+    }else{
+        let x = Number(tabuada.value);
+        resolucao.innerHTML = ''
+        for(let contador = 1;contador <= 10; contador+=1){
+            let item = document.createElement('option')
+            let mult = x * contador
+            resolucao.innerHTML += `${x} x ${contador} = ${mult}<br>`;
+            select.appendChild(item);
+        }
+    }
+    resolucao.style.fontFamily = 'Arial';
+    resolucao.style.margin = '20px'
+}     

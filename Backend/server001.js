22e6354527fdc599f3b0ac2e505bde62b52424fe
Ariel Marinho <167530405+arieldreis@users.noreply.github.com>
2025-05-19 22:13:57 -0300

    Server js

--- Backend/server001.js
+++ Backend/server001.js
@@ -1,1 +1,11 @@
-import express
+import express from 'express';
+
+const PORT  = 3000;
+const app = express();
+
+app.get('/', (request, response) => {
+    response.send('<h2>Back-end System im Js</h2>')
+});
+app.listen(PORT, () => {
+    console.log(`Servidor escutando na porta ${PORT}`);
+});

562dd7c3986f47c192cc53d642b8a304967529ab
Ariel dos Reis Marinho <arieldreis@gmail.com>
2025-06-04 20:25:36 -0300

    IMC POO

---
+++ ProgramaWebII/imcPOO/imcPOO/numerico.js
@@ -0,0 +1,8 @@
+function numerico(valor){
+    var num = valor.toString().replace(',','.');
+    if(isNaN(num)||num==''){
+        console.error('Somente valores numéricos são aceitos.');
+        return null;
+    }
+    return Number(num);
+}

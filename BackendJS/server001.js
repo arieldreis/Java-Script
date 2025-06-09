ab1ce6255d16c0d978a5c566436fec9ff92c667e
Ariel dos Reis Marinho <arieldreis@gmail.com>
2025-06-09 20:22:27 -0300

    Back-end system with js

--- BackendJS/server001.js
+++ BackendJS/server001.js
@@ -4,7 +4,7 @@ const PORT  = 3000;
 const app = express();
 
 app.get('/', (request, response) => {
-    response.send('<h2>Back-end System im Js</h2>')
+    response.send('<h2>Back-end System in Java-Script.</h2>')
 });
 app.listen(PORT, () => {
     console.log(`Servidor escutando na porta ${PORT}`);

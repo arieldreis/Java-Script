c6ad88ff1e1185dd16838c965bc90c807de33500
Ariel dos Reis Marinho <arieldreis@gmail.com>
2025-07-11 22:37:29 -0300

    Update the webSockets link

--- projetinhos/HashZapp/script.js
+++ projetinhos/HashZapp/script.js
@@ -10,7 +10,7 @@ const recebimento = document.querySelector('.message_received');
 const mensagens = document.getElementById('message_input');
 
 // Connection opened
-const ws = new WebSocket('ws://localhost:3010');
+const ws = new WebSocket('wss://12feb33f871e.ngrok-free.app');
 ws.onopen = () => {
     console.log("Connect to server.");
 }
